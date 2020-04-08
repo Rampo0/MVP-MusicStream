1c8c65578e058353084de1042b069abb4abc2311
Ammar Dwi Anwari <weissman1505199@gmail.com>
2020-04-08 18:56:31 +0700

    UI refurbish and binding backend with frontend

---
+++ app/src/main/java/me/rampoo/musicstream/baseactivity/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package me.rampoo.musicstream.baseactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
---
+++ app/src/main/java/me/rampoo/musicstream/data/model/Artist.kt
@@ -0,0 +1,4 @@
+package me.rampoo.musicstream.data.model
+
+class Artist {
+}
